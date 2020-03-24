6267338337cfb220c36472afd82a0289df337e86
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-03-24 11:45:50 -0400

    with correct models

---
+++ server/models/comment.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  title: String,
+  description: String,
+  date_created: Date,
+  tags: Array,
+  author: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  comments: Array
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
---
+++ server/models/task.js
@@ -0,0 +1,13 @@
+  
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TaskSchema = new Schema({
+  title: String,
+  date_created: Date,
+  assigned_to: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  due_date: Date,
+  completed: Boolean
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
