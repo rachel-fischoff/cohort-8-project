fb6584b3bd4115c1c6c32b38d9644a32c58168bf
BENZABOSS <joyceben33@gmail.com>
2020-03-24 14:44:12 -0400

    Added basic get route for todos.

--- server/routes/main.js
+++ server/routes/main.js
@@ -5,7 +5,7 @@ const Todo = require('../models/todo')
 
 
 //route for getting a single to do 
-router.get('/projects/:todo', (req, res) =>{
+router.get('/projects/:todo', (req, res) => {
     
     Todo.findOne({_id: req.params.todo})
       .exec((err, todo) => {
@@ -15,6 +15,39 @@ router.get('/projects/:todo', (req, res) =>{
               res.send(todo)
           }
       })
-  })
+})
 
-  modules.exports = router; 
+// This returns all the Todos in the DB
+router.get('projects/todos', (req, res) => {
+
+    Todo
+        .find()
+        .exec((err, todos) => {
+            Todo.countDocuments().exec((err, count) => {
+                if (err) return next(err)
+
+                res.send({
+                    todos: todos,
+                    todoCount: count
+                })
+            })
+        })
+
+})
+
+router.post('projects/todos', (req, res) => {
+
+})
+
+// Delete todo based on id in the query params
+router.delete('projects/:todo', (req, res) => {
+
+})
+
+// Update todo based on id in the query params
+router.put('projects/:todo', (req, res) => {
+
+})
+
+
+modules.exports = router; 
