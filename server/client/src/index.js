018d4e70ed63cc8ec29def03d42f845b9f37614f
Hannah Cline <hannahcline@Hannahs-MacBook-Air.local>
2020-03-24 16:36:13 -0400

    fixed landing page route

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { render } from "react-dom";
 import Nav from "./components/Nav";
 import App from './components/App';
+import Home from './components/Home';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
@@ -23,7 +24,7 @@ render(
         <Nav />
         <App>
           <Switch>
-            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/home" component={Home} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/groups" component={Groups} />
             <Route exact path="/calendar" component={ReactCalendar} />
