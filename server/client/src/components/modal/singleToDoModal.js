5851eee55086320c8e1939dbaf2626277dbb35aa
Katy Andersen <katherineandersen@sandiego.edu>
2020-03-25 13:45:46 -0400

    changes to single todo component

--- server/client/src/components/modal/singleToDoModal.js
+++ server/client/src/components/modal/singleToDoModal.js
@@ -1,37 +1,36 @@
-  import React, { Component, Fragment, useState } from 'react';
-  import Modal from 'react-bootstrap/Modal'
-  import './modal.css';
+import React, { Component, Fragment, useState } from 'react';
+import Modal from 'react-bootstrap/Modal'
+import SingleTodo from '../SingleTodo'
+import './modal.css';
+
+
+function SingleToDoModal() {
+
+    const [show, setShow] = useState(false);
   
-  function SingleToDoModal() {
-      const [show, setShow] = useState(false);
-    
-      return (
-        <>
-          <h5 variant="primary" onClick={() => setShow(true)}>
-           Single To-Do Header
-          </h5>
-    
-          <Modal className="modal-background-color"
-              backdrop="true"
-              size="xl"
-            show={show}
-            onHide={() => setShow(false)}
-            dialogClassName="modal-90w"
-            aria-labelledby="example-custom-modal-styling-title"
-          >
-            <Modal.Header closeButton >
-              <Modal.Title id="modal-title"  >
-                To-Do's header
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <p>
-                This is ONE single lonely To-DO
-              </p>
-            </Modal.Body>
-          </Modal>
-        </>
-      );
-    }
+    return (
+      <>
+        <h5 variant="primary" onClick={() => setShow(true)}>
+        <SingleTodo />
+        </h5>
   
-    export default SingleToDoModal;
+        <Modal className="modal-background-color"
+            backdrop="true"
+            size="xl"
+          show={show}
+          onHide={() => setShow(false)}
+          dialogClassName="modal-90w"
+          aria-labelledby="example-custom-modal-styling-title"
+        >
+          <Modal.Header closeButton >
+            <Modal.Title id="modal-title"  >
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          </Modal.Body>
+        </Modal>
+      </>
+    );
+  }
+
+  export default SingleToDoModal;
