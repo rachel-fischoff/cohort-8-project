62ab7b67ac4605387570b18b9be217dfb56d580e
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-03-26 09:51:35 -0400

    newTeamPop added few things. not finished

--- server/client/src/components/popover/newTeamPop.js
+++ server/client/src/components/popover/newTeamPop.js
@@ -1,56 +1,69 @@
 import React from "react";
+import * as actions from '../../actions/index';
+import { connect } from 'react-redux';
 import { MDBPopover, MDBPopoverBody, MDBPopoverHeader, MDBBtn, MDBContainer } from "mdbreact";
 import Badge from 'react-bootstrap/Badge'
 import './pop.css'
 
-const PopoverPage = () => {
+
+
+class PopoverPage extends React.Component {
+  
+  //constructs a new state for the List 
+  constructor(props) {
+      super(props)
+
+      
+      this.state = {
+          name: '',
+          description: '',
+      }
+      //binds the functions
+      this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  //called when user hits submit
+  handleSubmit() {
     
-    const createdGroupName = '';
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('click')
+  }
+
+  render() {
+      return (
+        <MDBContainer>
+             <div  >
         
-        console.log('created group name: ', createdGroupName)
-    }
-
-    const handleCancel = (e) => {
-        e.preventDefault();
-        console.log('clicked cancel', e)
+              <MDBPopover 
+                placement="right"
+                popover
+                clickable
+                id="popper3"
+              >
+                <MDBBtn><button id="but-pop" className="btn btn-success new-button" data-toggle="popover"
+                    >+ New</button></MDBBtn>
+                
+                  
+                  <MDBPopoverBody>
+                    <form  >
+                    <input type="newName" id="newGroupN"
+                    onChange={event => this.setState({name: event.target.value})}  />
+                        <div>
+                        <button className="btn-success" type="onSubmit" onClick={this.handleSubmit} >Save</button> &nbsp;
+                        <button className="btn-danger" type="onSubmit" >Cancel</button>
+                        </div>
+                    </form>
+                  </MDBPopoverBody>
+                
+              </MDBPopover>
         
-       
-    }
-
-  return (
-    <MDBContainer>
-      <div  >
-
-        <MDBPopover id="qwe"
-          placement="right"
-          popover
-          clickable
-          id="popper3"
-        >
-          <MDBBtn><button id="but-pop" className="btn btn-success new-button" data-toggle="popover"
-              >+ New</button></MDBBtn>
-          
-            
-            <MDBPopoverBody>
-              <form  onSubmit={handleSubmit}>
-              <input type="newName" id="newGroupN"  />
-                  <div>
-                  <button className="btn-success" type="onSubmit" >Save</button> &nbsp;
-                  <button className="btn-danger" type="onSubmit" onClick={handleCancel}>Cancel</button>
-                  </div>
-              </form>
-            </MDBPopoverBody>
-          
-        </MDBPopover>
-
-        
-      </div>
-    </MDBContainer>
-  );
+              
+            </div>
+          </MDBContainer>
+        );
+  }
 }
 
-export default PopoverPage;
+
+export default connect(
+  null,
+  actions
+)(PopoverPage);
+
