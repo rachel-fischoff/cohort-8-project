6a9c87d8e76e9e72930cb4bc900e843f2848956a
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-03-27 10:08:38 -0400

    find button in NavBar alligned

--- server/client/src/components/calendar/calendar.js
+++ server/client/src/components/calendar/calendar.js
@@ -1,14 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { render } from "react-dom";
 import { connect } from "react-redux";
 import SingleCalendar from './singleCalendar'
 import './calendar.css';
-
-import * as actions from '../../actions';
-import _ from 'lodash';
-
 import { Button, Modal,  ModalBody, ModalFooter, NavLink } from 'reactstrap';
 import { Image } from 'react-bootstrap'
-
 
 
 
@@ -48,43 +44,32 @@ class ReactCalendar extends React.Component {
         });
       }
 
-    componentDidMount() {
-        //fetches the tasks
-        this.props.fetchSchedule(this.props.groupId)
+    async componentDidMount() {
+        console.log(this.props)
+
+        let todos = this.props.group.todos;
+        console.log(todos.task)
+        todos.map(task => (
+            console.log('map ', task.tasks)
+        ))
     }
 
-
-  sortAndRenderDates=()=>{
-    //orders the due dates
-    let tasks = []
-    let sortedDates = []
-    _.map(this.props.tasks, function(task){sortedDates.push(task.due_date)})
-    sortedDates.sort()
-    sortedDates.reverse()
-
-  
-    //ugly loop to match the order dates array to the whole task
-    for (let i = 0; i< sortedDates.length; i++){
-        for (let j=0; j< this.props.tasks.length; j++){
-            if (sortedDates[i] == this.props.tasks[j].due_date){
-                tasks.push(this.props.tasks[j])
-            }
-        }
-    }
-        if (this.props.tasks === undefined) {
+    renderTodos() {
+        if (this.props.group.todos === undefined) {
             return (
                 <div>Loading ... </div>
             )
         } else {
             return (
-                    <div>
+                this.props.group.todos.map(todo => (
+                    <div className=" row">
+                        <div className="col fixed-center" id="calendarCenter">
                         {
-                            tasks.map(function(t){
-                                return(
+                            todo.tasks.map(task => (
                                 <div className="todo-tasks fixed-center">
                                     <input className="d-flex position-left form-check-input" type="checkbox"  id="defaultCheck1"></input>
-                                    <NavLink className="position-absolute float-left form-check-label" for="defaultCheck1">{t.due_date}&nbsp;&nbsp;{t.title}</NavLink>
-                                        {/* <Modal
+                                    <NavLink onClick={this.toggleNested} className="position-absolute float-left form-check-label" for="defaultCheck1">{task.due_date.split('T')[0]}&nbsp;&nbsp;{task.title}</NavLink>
+                                        <Modal
                                         isOpen={this.state.nestedModal}
                                         toggle={this.toggleNested}
                                         onClosed={this.state.closeAll ? this.toggle : undefined}
@@ -95,10 +80,10 @@ class ReactCalendar extends React.Component {
                                                 <div id="taskLeft" className="todo-tasks">
                                                 <div className="row">
                                                 <input  class="form-check-input" type="checkbox" value="" id="defaultCheck1"></input>
-                                                <label  class="form-check-label" for="defaultCheck1">{t.title}</label>
+                                                <label  class="form-check-label" for="defaultCheck1">{task.title}</label>
                                                 <span></span>
-                                                <p id="insideCal" className="profile-name">Assigned To: {t.assigned_to.profile_name}</p>
-                                                <span><Image src={t.assigned_to.profile_pic_url} alt="user avatar" roundedCircle fluid width="25px" height='25px'/></span>
+                                                <p id="insideCal" className="profile-name">Assigned To: {task.assigned_to.profile_name}</p>
+                                                <span><Image src={task.assigned_to.profile_pic_url} alt="user avatar" roundedCircle fluid width="25px" height='25px'/></span>
                                                 </div>
                                                 </div>
                                         </ModalBody>
@@ -110,24 +95,27 @@ class ReactCalendar extends React.Component {
                                             All Done
                                             </Button>
                                         </ModalFooter>
-                                        </Modal> */}
+                                        </Modal>
                                     <br></br>
                                 </div>
-                                )
-                            
-                            })
+
+                            ))
                         }
                         <br></br>
                         </div>
-                )
+                    </div>
+                ))
+            )
         }
     }
-    
-  
+
     render() {
         return (
             <div>
-                <div>{this.sortAndRenderDates()}</div>
+                <SingleCalendar
+
+                />
+                <div>{this.renderTodos()}</div>
             </div>
         )
     }
@@ -136,8 +124,10 @@ class ReactCalendar extends React.Component {
 
 function mapStateToProps(state) {
     return ({
-        tasks: state.schedule
+        homePage: state.home,
+        user: state.user,
+        group: state.group
     })
 }
 
-export default connect(mapStateToProps, actions)(ReactCalendar);
+export default connect(mapStateToProps)(ReactCalendar);
