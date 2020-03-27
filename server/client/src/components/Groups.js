b2fdf3c3431213bcda84904d1ce82fd02540fd10
BENZABOSS <joyceben33@gmail.com>
2020-03-27 12:06:55 -0400

    Fixed the overflow of content on group page

--- server/client/src/components/Groups.js
+++ server/client/src/components/Groups.js
@@ -7,28 +7,28 @@ import MessageBoardModal from '../components/modal/messageBoardModal';
 import './groups.css';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
-import {Image, Row, Container} from 'react-bootstrap'
+import { Image, Row, Container } from 'react-bootstrap'
 
 
 
-class Groups extends Component {  
+class Groups extends Component {
   constructor(props) {
     super(props)
     //for the modals
     this.state = {
-    viewingToDos: false,
-    events: [],
-    isLoading: false,
-    selectedEvent: null
-  }
+      viewingToDos: false,
+      events: [],
+      isLoading: false,
+      selectedEvent: null
+    }
 
-  //binds function to this state
-  this.renderPerson = this.renderPerson.bind(this)
-};
+    //binds function to this state
+    this.renderPerson = this.renderPerson.bind(this)
+  };
   //fetches data when loads
-  componentDidMount(){
+  componentDidMount() {
     this.props.fetchGroupDetails(this.props.match.params.groupId)
-  
+
   }
 
   viewToDoList = () => {
@@ -41,31 +41,31 @@ class Groups extends Component {
 
 
   renderPerson = (p) => {
-      return(
-        <Image src = {p.profile_pic_url} roundedCircle fluid width="50px" height='50px'/>
-      )
-    }
+    return (
+      <Image src={p.profile_pic_url} roundedCircle fluid width="50px" height='50px' />
+    )
+  }
 
 
-      render() {
-  
-        return (
-          <div className="groups-page">
-            <div className="row">
-              <br></br>
-               <div className="card-groups col-md-10 mx-auto">
-               <h1 className="card-title-groups text-center">{this.props.groupName}</h1>
-               <h4 className="card-title-groups text-center text-muted"> {this.props.groupDescription}</h4>
-               <Container>
-                <Row className="justify-content-md-center">
-                  {this.props.people.map(this.renderPerson)}
-                </Row>
-              </Container>
+  render() {
+
+    return (
+      <div className="groups-page app_body">
+        <div className="row ">
+          <br></br>
+          <div className="card-groups col-md-10 mx-auto">
+            <h1 className="card-title-groups text-center">{this.props.groupName}</h1>
+            <h4 className="card-title-groups text-center text-muted"> {this.props.groupDescription}</h4>
+            <Container>
+              <Row className="justify-content-md-center">
+                {this.props.people.map(this.renderPerson)}
+              </Row>
+            </Container>
 
 
-              <div className="card-body ">
-                <div className="row text-center ">
-
+            <div className="card-body ">
+              <div className="row text-center ">
+                <div classname="card__content">
                   <div className="col-md-4 ">
                     <div className="card-inner">
                       <CalendarModal groupId={this.props.match.params.groupId}><p>Calendar</p></CalendarModal>
@@ -73,46 +73,53 @@ class Groups extends Component {
 
                     </div>
                   </div>
+                </div>
 
-                  <div className="col-md-4">
-                    <div className="card-inner">
-                      <ToDoModal groupId={this.props.match.params.groupId}/>
-                      <hr />
+                <div className="card__content ">
+                  <div className="row text-center ">
+                    <div className="col-md-4">
+                      <div className="card-inner">
+                        <ToDoModal groupId={this.props.match.params.groupId} />
+                        <hr />
+                      </div>
                     </div>
                   </div>
+                </div>
 
+                <div className="card__content">
                   <div className="col-md-4">
                     <div className="card-inner">
                       <MessageBoardModal></MessageBoardModal>
                       <hr />
-                    <div className="card-body">
-                    <p className="card-text">Coming Soon!</p>
+                      <div className="card-body">
+                        <p className="card-text">Coming Soon!</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
+        </div>
       </div>
-    </div>
     );
   }
 }
 
 
-function mapStateToProps (state) {
-    return ({
-      people: state.group.people,
-      comments: state.group.comments,
-      todos: state.group.todos,
-      groupName: state.group.group_name,
-      groupDescription: state.group.group_description,
-      groupId: state.group._id
-    })};
-  
-  export default connect(
-    mapStateToProps,
-    actions
-  )(Groups);
- 
+function mapStateToProps(state) {
+  return ({
+    people: state.group.people,
+    comments: state.group.comments,
+    todos: state.group.todos,
+    groupName: state.group.group_name,
+    groupDescription: state.group.group_description,
+    groupId: state.group._id
+  })
+};
+
+export default connect(
+  mapStateToProps,
+  actions
+)(Groups);
